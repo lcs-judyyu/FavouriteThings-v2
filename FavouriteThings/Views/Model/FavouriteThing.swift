cbce2d3af87cb5d0c6445bf98d5dc8f9b85357c9
Judy <jyu@lcs.on.ca>
2021-11-05 09:54:17 -0400

    Add a group called Model and a file call FavouriteThing

---
+++ FavouriteThings/Views/Model/FavouriteThing.swift
@@ -0,0 +1,20 @@
+//
+//  FavouriteThing.swift
+//  FavouriteThings
+//
+//  Created by Judy Yu on 2021-11-05.
+//
+
+import SwiftUI
+
+struct FavouriteThing: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavouriteThing_Previews: PreviewProvider {
+    static var previews: some View {
+        FavouriteThing()
+    }
+}
