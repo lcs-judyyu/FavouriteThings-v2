845eb93d5de008e73a33b80417e31081d81d8478
Judy <jyu@lcs.on.ca>
2021-11-05 22:31:24 -0400

    Deleted five other views and make changes to the DetailView

--- FavouriteThings/Views/DetailView.swift
+++ FavouriteThings/Views/DetailView.swift
@@ -7,22 +7,24 @@
 
 import SwiftUI
 
-struct RamenView: View {
+struct DetailView: View {
+    
+    //MARK: Stored Properties
+    let item: FavouriteThing
+    
     var body: some View {
         ScrollView {
             VStack {
                 VStack(alignment: .leading){
-                    Image("Ramen")
+                    Image(item.imageName)
                         .resizable()
                         .scaledToFit()
                         .padding(20)
                     
-                    PhotoCaptionsView(caption: "A bowl of ramen that looks delicious")
+                    PhotoCaptionsView(caption: item.captions)
                 }
                 
-                Text("""
-Ramen is another dish I really like.\n\nLorem ipsum dolor sit amet, Dui sapien eget mi proin sed libero enim. Gravida rutrum quisque non tellus. Tristique senectus et netus et malesuada fames ac turpis. Lorem ipsum dolor sit amet consectetur adipiscing. Tellus integer feugiat scelerisque varius morbi enim nunc faucibus. Fringilla urna porttitor rhoncus dolor purus non enim praesent elementum. Nisi porta lorem mollis aliquam ut porttitor leo a.
-""")
+                Text(item.text)
                     .frame(width: 270, height: 800, alignment: .top)
                     .padding(20)
                 
@@ -33,10 +35,10 @@ Ramen is another dish I really like.\n\nLorem ipsum dolor sit amet, Dui sapien e
     }
 }
 
-struct RamenView_Previews: PreviewProvider {
+struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            RamenView()
+            DetailView(item: list)
         }
     }
 }
--- FavouriteThings/Views/Model/FavouriteThing.swift
+++ FavouriteThings/Views/Model/FavouriteThing.swift
@@ -14,25 +14,24 @@ struct FavouriteThing {
     let title: String
     let navigationTitle: String
     let captions: String
-    let credit: String
     let text: String
     
     let listOfItems = [
-       FavouriteThing(imageName: "Chinchilla", title: "Chinchilla", navigationTitle: "Chinchilla", captions: "A Chinchilla", credit: "Online resource", text: "I have a chinchilla named Chocolate. (Not the one above) He is so cute!")
+       FavouriteThing(imageName: "Chinchilla", title: "Chinchilla", navigationTitle: "Chinchilla", captions: "A Chinchilla", text: "I have a chinchilla named Chocolate. (Not the one above) He is so cute!")
        ,
-       FavouriteThing(imageName: "IceCream", title: "Ice Cream", navigationTitle: "IceCream", captions: "Ice cream", credit: "Online resource", text: "Ice cream is my favourite dessert")
+       FavouriteThing(imageName: "IceCream", title: "Ice Cream", navigationTitle: "IceCream", captions: "Ice cream", text: "Ice cream is my favourite dessert")
         ,
        
-       FavouriteThing(imageName: "Lavender", title: "Lavender", navigationTitle: "Lavender", captions: "Lavender field", credit: "Online resource", text: "I LOVE lavender scent :)")
+       FavouriteThing(imageName: "Lavender", title: "Lavender", navigationTitle: "Lavender", captions: "Lavender field", text: "I LOVE lavender scent :)")
         ,
-       FavouriteThing(imageName: "Hotpot", title: "Hot pot", navigationTitle: "Hot pot", captions: "Hotpot", credit: "Online resource", text: "Hotpot is my favourite Chinese dish because I can put every ingredient I like in differemnt soup bases. My favourite soup base for hotpot is the spicy one.\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ")
+       FavouriteThing(imageName: "Hotpot", title: "Hot pot", navigationTitle: "Hot pot", captions: "Hotpot", text: "Hotpot is my favourite Chinese dish because I can put every ingredient I like in differemnt soup bases. My favourite soup base for hotpot is the spicy one.\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ")
         ,
        FavouriteThing(imageName: "Ramen", title: "Ramen",
-                      navigationTitle: "Ramen", captions: "A bowl of delicious ramen", credit: "Online resource", text: """
+                      navigationTitle: "Ramen", captions: "A bowl of delicious ramen", text: """
                       Ramen is another dish I really like.\n\nLorem ipsum dolor sit amet, Dui sapien eget mi proin sed libero enim. Gravida rutrum quisque non tellus. Tristique senectus et netus et malesuada fames ac turpis. Lorem ipsum dolor sit amet consectetur adipiscing.
                       """)
         ,
-       FavouriteThing(imageName: "Roses", title: "Roses", navigationTitle: "Roses", captions: "Beautiful roses", credit: "Online resource", text: "Roses are always my first choice for decorating my space.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+       FavouriteThing(imageName: "Roses", title: "Roses", navigationTitle: "Roses", captions: "Beautiful roses", text: "Roses are always my first choice for decorating my space.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
         
     ]
 }
